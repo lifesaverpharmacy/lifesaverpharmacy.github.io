1e66d55f95b393ec3098019a09c3ccb5c68ae579
sreerajpjayaraj <sreerajpjayaraj@gmail.com>
2025-05-09 16:38:30 +0530

    Fixed UI issues in small screen
    Issues related to homepage carousel and  images in small screen are fixed.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,42 @@
 (function ($) {
-  function initSlick () {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 7,
-        slidesToScroll: 3
-      });
+  function initSlick() {
+    $(".multiple-items").slick({
+      infinite: true,
+      slidesToShow: 7,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true,
+          },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
+    });
   }
   $(document).ready(function () {
     initSlick();
